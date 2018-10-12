e485c1b8113cdb484906fca6cfd36a8bc4a175b6
Andrii Havrilin <andrew.rose.ua@gmail.com>
2018-10-12 17:26:24 +0300

    Tags input функціонує аналогічно прикладу, невдала спроба реалізації збереження тагів закоментована в коді

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
 'use strict';
 
-const $tagsInput = document.getElementById('tags-input');
-const $tagsList = document.getElementById('tags-list');
-const tagsArr = ['1', 'super tag'];
+const $tagsInput1 = document.getElementById('tags-input1');
+const $tagsList1 = document.getElementById('tags-list1');
+const tagsArr1 = ['1', 'super tag'];
+const $tagsInput2 = document.getElementById('tags-input2');
+const $tagsList2 = document.getElementById('tags-list2');
+const tagsArr2 = [];
 
-$tagsInput.addEventListener('keyup', ({key, target}) => {
+//if (localStorage.getItem("tagsArr2") === null) { 
+//  localStorage.setItem("tagsArr2", "");
+//}
+//else {
+//tagsArr2 =  localStorage.getItem("tagsArr2").split(', ');
+//}
+
+$tagsInput1.addEventListener('keyup', ({key, target}) => {
   if (key === 'Enter' && target.value.trim()) {
     const $el = createTagElement(target.value);
-    const isElementExist = tagsArr.includes(target.value);
+    const isElementExist = tagsArr1.includes(target.value);
 
     if (!isElementExist) {
-      $tagsList.appendChild($el);
-      tagsArr.push(target.value);
+      $tagsList1.appendChild($el);
+      tagsArr1.push(target.value);
       target.value = '';
     }
   }
 });
 
-$tagsList.addEventListener('click', event => {
+$tagsInput2.addEventListener('keyup', ({key, target}) => {
+  if (key === 'Enter' && target.value.trim()) {
+    const $el = createTagElement(target.value);
+    const isElementExist = tagsArr2.includes(target.value);
+    if (!isElementExist) {
+      $tagsList2.appendChild($el);
+      tagsArr2.push(target.value);
+      //localStorage.setItem("tagsArr2", tagsArr2.join(', ') + target.value );
+      target.value = '';
+    }
+  }
+});
+
+$tagsList1.addEventListener('click', event => {
   const {target} = event;
   const isRemoveBtn = target.classList.contains('remove-btn');
   const $foo = target.parentElement.parentElement;
@@ -25,9 +48,18 @@ $tagsList.addEventListener('click', event => {
 
   if (isRemoveBtn) {
     $foo.removeChild($tag);
+  }
+});
 
-    // TODO: remove element from array
-    // tagsArr.splice();
+$tagsList2.addEventListener('click', event => {
+  const {target} = event;
+  const isRemoveBtn = target.classList.contains('remove-btn');
+  const $foo = target.parentElement.parentElement;
+  const $tag = target.parentElement;
+
+  if (isRemoveBtn) {
+    $foo.removeChild($tag);
+    //tagsArr2.splice(target.value);
   }
 });
 
@@ -44,10 +76,15 @@ const createTagElement = content => {
 };
 
 const init = () => {
-  tagsArr.forEach(tagValue => {
+  tagsArr1.forEach(tagValue => {
     const $el = createTagElement(tagValue);
 
-    $tagsList.appendChild($el);
+    $tagsList1.appendChild($el);
+  });
+  tagsArr2.forEach(tagValue => {
+    const $el = createTagElement(tagValue);
+
+    $tagsList2.appendChild($el);
   });
 };
 
